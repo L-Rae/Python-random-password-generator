ee2ce892014253f57f081c977e87ac863ef77cc9
Rea Revilleza <125110377+L-Rae@users.noreply.github.com>
2023-02-11 21:24:27 +0800

    add comments to explain the code

--- randompass.py
+++ randompass.py
@@ -1,10 +1,16 @@
-import random
 
+#import the random module 
+import random 
+
+#create a list of characters to choose from 
 characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+=-"
 
-password = ""
+#create an empty string to store the password 
+password = "" 
 
+#generate a random password with 8 characters 
 for i in range(8): 
     password += random.choice(characters) 
     
-print("Your new password is:", password)    
+#print the generated password 
+print("Your new password is:", password)
