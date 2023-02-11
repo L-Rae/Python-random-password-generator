8ac5d30850d9fac47c2d39e6a2a76b9d54a41738
Dius Montenegro <114764373+DreaUltimate@users.noreply.github.com>
2023-02-11 21:20:07 +0800

    completing the code

--- randompass.py
+++ randompass.py
@@ -1,1 +1,10 @@
 import random
+
+characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+=-"
+
+password = ""
+
+for i in range(8): 
+    password += random.choice(characters) 
+    
+print("Your new password is:", password)    
